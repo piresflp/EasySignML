7680dcb9979f2f4b3c0fbf25d6a1fdd0362ad425
Felipe Pires <flpiresa@gmail.com>
2021-08-23 11:54:08 -0300

    import main file

---
+++ action_recognition.py
@@ -0,0 +1,117 @@
+# pip install tensorflow==2.4.1 tensorflow-gpu==2.4.1 opencv-python mediapipe sklearn matplotlib
+# Importando bibliotecas necessárias
+import cv2
+import numpy as np
+import os
+from matplotlib import pyplot as plt
+import time
+import mediapipe as mp
+
+mp_holistic = mp.solutions.holistic  # modelo holistico
+mp_drawing = mp.solutions.drawing_utils  # desenhando o modelo
+
+DATA_PATH = os.path.join('MP_Data')  # caminho para os dados exporatods
+no_sequences = 30  # 30 videos de dado para cada ação
+sequence_length = 30  # cada video com 30 frames
+
+# ações que tentaremos detectar
+actions = np.array(['hello', 'thanks', 'iloveyou'])
+
+for action in actions:
+    for sequence in range(no_sequences):
+        try:
+            os.makedirs(os.path.join(DATA_PATH, action, str(sequence)))
+        except:
+            pass
+
+
+def mediapipe_detection(image, model):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # conversão das cores
+    image.flags.writeable = False  # imagem não é mais alteravel
+    results = model.process(image)  # realiza o reconhecimento
+    image.flags.writeable = True  # imagem alteravel
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)  # conversão das cores
+    return image, results
+
+
+def draw_landmarks(image, results):
+    # Desenha pontos do rosto
+    mp_drawing.draw_landmarks(
+        image, results.face_landmarks, mp_holistic.FACE_CONNECTIONS)
+    # Desenha pontos da pose
+    mp_drawing.draw_landmarks(
+        image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
+    # Desenha pontos da mão esquerda
+    mp_drawing.draw_landmarks(
+        image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+    # Desenha pontos da mão direita
+    mp_drawing.draw_landmarks(
+        image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+
+
+def draw_styled_landmarks(image, results):
+    # Desenha pontos do rosto
+    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACE_CONNECTIONS,
+                              mp_drawing.DrawingSpec(
+                                  color=(80, 110, 10), thickness=1, circle_radius=1),
+                              mp_drawing.DrawingSpec(
+                                  color=(80, 256, 121), thickness=1, circle_radius=1)
+                              )
+    # Desenha pontos da pose
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+                              mp_drawing.DrawingSpec(
+                                  color=(80, 22, 10), thickness=2, circle_radius=4),
+                              mp_drawing.DrawingSpec(
+                                  color=(80, 44, 121), thickness=2, circle_radius=2)
+                              )
+    # Desenha pontos da mão esquerda
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                              mp_drawing.DrawingSpec(
+                                  color=(121, 22, 76), thickness=2, circle_radius=4),
+                              mp_drawing.DrawingSpec(
+                                  color=(121, 44, 250), thickness=2, circle_radius=2)
+                              )
+    # Desenha pontos da mão direita
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                              mp_drawing.DrawingSpec(
+                                  color=(245, 117, 66), thickness=2, circle_radius=4),
+                              mp_drawing.DrawingSpec(
+                                  color=(245, 66, 230), thickness=2, circle_radius=2)
+                              )
+
+
+def extract_keypoints(results):
+    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten(
+    ) if results.pose_landmarks else np.zeros(33*4)  # extrai pontos da pose
+    face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten(
+    ) if results.face_landmarks else np.zeros(468*3)  # pontos da face
+    lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten(
+    ) if results.left_hand_landmarks else np.zeros(21*3)  # pontos da mão esquerda
+    rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten(
+    ) if results.right_hand_landmarks else np.zeros(21*3)  # pontos da mão direita
+    # une todos os pontos do corpo
+    return np.concatenate([pose, face, lh, rh])
+
+
+# Configurando a captura da webcam
+cap = cv2.VideoCapture(0)
+# configura modelo mediapipe
+with mp_holistic.Holistic(min_detection_confidence=0.3, min_tracking_confidence=0.5) as holistic:
+    while cap.isOpened():
+        ret, frame = cap.read()  # lê o frame atual
+
+        image, results = mediapipe_detection(frame, holistic)
+        print(results)
+
+        # Desenha pontos
+        draw_styled_landmarks(image, results)
+
+        cv2.imshow('OpenCV Feed', image)  # mostra o frame na tela
+
+        # EXTRAINDO KEYPOINTS
+
+        # fecha o programa ao pressionar a tecla 'Q'
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
