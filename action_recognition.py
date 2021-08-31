8ac2167095b99991669771ae97c6d0fce6fb84cd
Felipe Pires <flpiresa@gmail.com>
2021-08-31 19:32:20 -0300

    training script

--- action_recognition.py
+++ action_recognition.py
@@ -1,5 +1,10 @@
 # pip install tensorflow==2.4.1 tensorflow-gpu==2.4.1 opencv-python mediapipe sklearn matplotlib
 # Importando bibliotecas necessárias
+from tensorflow.keras.callbacks import TensorBoard
+from tensorflow.keras.layers import LSTM, Dense
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
 import cv2
 import numpy as np
 import os
@@ -13,122 +18,39 @@ mp_drawing = mp.solutions.drawing_utils  # desenhando o modelo
 DATA_PATH = os.path.join('MP_Data')  # caminho para os dados exporatods
 no_sequences = 30  # 30 videos de dado para cada ação
 sequence_length = 30  # cada video com 30 frames
+actions = np.array(['ola', 'eu_te_amo', 'boa_noite']
+                   )  # ações que tentaremos detectar
 
-# ações que tentaremos detectar
-actions = np.array(['hello', 'thanks', 'iloveyou'])
-
+label_map = {label: num for num, label in enumerate(actions)}
+sequences, labels = [], []
 for action in actions:
     for sequence in range(no_sequences):
-        try:
-            os.makedirs(os.path.join(DATA_PATH, action, str(sequence)))
-        except:
-            pass
+        window = []
+        for frame_num in range(sequence_length):
+            res = np.load(os.path.join(DATA_PATH, action, str(
+                sequence), "{}.npy".format(frame_num)))
+            window.append(res)
+        sequences.append(window)
+        labels.append(label_map[action])
+
+X = np.array(sequences)  # videos testados
+y = to_categorical(labels).astype(int)  # parâmetros de teste
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.05)
 
 
-def mediapipe_detection(image, model):
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # conversão das cores
-    image.flags.writeable = False  # imagem não é mais alteravel
-    results = model.process(image)  # realiza o reconhecimento
-    image.flags.writeable = True  # imagem alteravel
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)  # conversão das cores
-    return image, results
+log_dir = os.path.join('Logs')
+tb_callback = TensorBoard(log_dir=log_dir)
+
+model = Sequential()
+model.add(LSTM(64, return_sequences=True,
+          activation='relu', input_shape=(30, 1662)))
+model.add(LSTM(128, return_sequences=True, activation='relu'))
+model.add(LSTM(64, return_sequences=False, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(actions.shape[0], activation='softmax'))
 
 
-def draw_landmarks(image, results):
-    # Desenha pontos do rosto
-    mp_drawing.draw_landmarks(
-        image, results.face_landmarks, mp_holistic.FACE_CONNECTIONS)
-    # Desenha pontos da pose
-    mp_drawing.draw_landmarks(
-        image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
-    # Desenha pontos da mão esquerda
-    mp_drawing.draw_landmarks(
-        image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
-    # Desenha pontos da mão direita
-    mp_drawing.draw_landmarks(
-        image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
-
-
-def draw_styled_landmarks(image, results):
-    # Desenha pontos do rosto
-    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACE_CONNECTIONS,
-                              mp_drawing.DrawingSpec(
-                                  color=(80, 110, 10), thickness=1, circle_radius=1),
-                              mp_drawing.DrawingSpec(
-                                  color=(80, 256, 121), thickness=1, circle_radius=1)
-                              )
-    # Desenha pontos da pose
-    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
-                              mp_drawing.DrawingSpec(
-                                  color=(80, 22, 10), thickness=2, circle_radius=4),
-                              mp_drawing.DrawingSpec(
-                                  color=(80, 44, 121), thickness=2, circle_radius=2)
-                              )
-    # Desenha pontos da mão esquerda
-    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
-                              mp_drawing.DrawingSpec(
-                                  color=(121, 22, 76), thickness=2, circle_radius=4),
-                              mp_drawing.DrawingSpec(
-                                  color=(121, 44, 250), thickness=2, circle_radius=2)
-                              )
-    # Desenha pontos da mão direita
-    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
-                              mp_drawing.DrawingSpec(
-                                  color=(245, 117, 66), thickness=2, circle_radius=4),
-                              mp_drawing.DrawingSpec(
-                                  color=(245, 66, 230), thickness=2, circle_radius=2)
-                              )
-
-
-def extract_keypoints(results):
-    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten(
-    ) if results.pose_landmarks else np.zeros(33*4)  # extrai pontos da pose
-    face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten(
-    ) if results.face_landmarks else np.zeros(468*3)  # pontos da face
-    lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten(
-    ) if results.left_hand_landmarks else np.zeros(21*3)  # pontos da mão esquerda
-    rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten(
-    ) if results.right_hand_landmarks else np.zeros(21*3)  # pontos da mão direita
-    # une todos os pontos do corpo
-    return np.concatenate([pose, face, lh, rh])
-
-
-# Configurando a captura da webcam
-cap = cv2.VideoCapture(0)
-# configura modelo mediapipe
-with mp_holistic.Holistic(min_detection_confidence=0.3, min_tracking_confidence=0.5) as holistic:
-    for action in actions:  # loop pelas ações
-        for sequence in range(no_sequences):  # loop pelas sequências (vídeos)
-            # loop pelos frames dos videos
-            for frame_num in range(sequence_length):
-
-                ret, frame = cap.read()  # lê o frame atual
-
-                image, results = mediapipe_detection(frame, holistic)
-                print(results)
-
-                # Desenha pontos
-                draw_styled_landmarks(image, results)
-
-                if frame_num == 0:
-                    cv2.putText(image, 'STARTING COLLECTION', (120, 200),
-                                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 4, cv2.LINE_AA)
-                    cv2.putText(image, 'Collecting frames for {} Video Number {}'.format(action, sequence), (15, 12),
-                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
-                    cv2.imshow('OpenCV Feed', image)
-                    cv2.waitKey(1500)
-                else:
-                    cv2.putText(image, 'Collecting frames for {} Video Number {}'.format(action, sequence), (15, 12),
-                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
-                    cv2.imshow('OpenCV Feed', image)
-
-                keypoints = extract_keypoints(results)
-                npy_path = os.path.join(
-                    DATA_PATH, action, str(sequence), str(frame_num))
-                np.save(npy_path, keypoints)
-
-                # fecha o programa ao pressionar a tecla 'Q'
-                if cv2.waitKey(10) & 0xFF == ord('q'):
-                    break
-    cap.release()
-    cv2.destroyAllWindows()
+model.compile(optimizer='Adam', loss='categorical_crossentropy',
+              metrics=['categorical_accuracy'])
+model.fit(X_train, y_train, epochs=2000, callbacks=[tb_callback])
